4a7381e08596b5d33466abd728b32e71bd7331fa
swathika2008 <swathikasenthilmurugan2008@gmail.com>
2026-02-10 21:51:32 +0530

    Add files via upload
    control statements

---
+++ practice(java)/Control statements/switchcase.java
@@ -0,0 +1,15 @@
+//SWITCH CASE
+class switchcase {
+    public static void main(String[] args) {
+int a = 7;
+int b = 18;
+char op = '-';
+switch (op){
+case '+':System.out.println(a+b);break;
+case '-':System.out.println(a-b);break;
+case '*':System.out.println(a*b);break;
+case '/':System.out.println(a/b);break;
+
+}
+}
+}
