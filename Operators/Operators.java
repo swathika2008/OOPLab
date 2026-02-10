9aeadc2f67b8a71803b2a58c84bb93536096c40b
swathika2008 <swathikasenthilmurugan2008@gmail.com>
2026-02-10 22:03:57 +0530

    Add files via upload
    operators

---
+++ Operators/Operators.java
@@ -0,0 +1,26 @@
+public class Operators{
+public static void main(String[]args){
+int a = 6;
+int b = 3;
+boolean x = true;
+boolean y = false;
+System.out.println("a+b:"+(a+b));
+System.out.println("a-b:"+(a-b));
+System.out.println("a*b:"+(a*b));
+System.out.println("a / b:"+(a/b));
+System.out.println("a%b:"+(a%b));
+System.out.println("a > b:"+(a>b));
+System.out.println("a < b:"+ (a<b));
+System.out.println("a = b:"+(a==b));
+System.out.println("a!=b:"+(a!=b));
+System.out.println("x&y:"+(x&&y));
+System.out.println("x or y:"+(x||y));
+System.out.println("~x"+(!x));
+
+
+
+
+
+
+}
+}
